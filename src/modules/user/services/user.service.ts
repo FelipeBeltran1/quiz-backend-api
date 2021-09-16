de4f6ed4346bbaea15bf50843c086e83dfcdbf57
Andres Cadena <andrescadena0607@gmail.com>
2021-09-16 18:15:07 -0500

    user service crud

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto, UpdateUserDto } from '../dto/user.dto';
 
 @Injectable()
-export class SerivesService {}
+export class SerivesService {
+  constructor(
+    @InjectRepository(User, 'users')
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    try {
+      const user = await this.userRepository.findOneOrFail(id);
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  create(payload: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create(payload);
+    return this.userRepository.save(newUser);
+  }
+
+  async update(id: number, payload: UpdateUserDto): Promise<User> {
+    const user = await this.findOne(id);
+    user.email = payload.email;
+    return this.userRepository.save(user);
+  }
+
+  async delete(id: number): Promise<User> {
+    const user = await this.findOne(id);
+    return this.userRepository.remove(user);
+  }
+}
