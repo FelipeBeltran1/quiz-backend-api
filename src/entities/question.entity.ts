32f30b730bafc57628bc0280c2dc95ce1385a6a0
Andres Cadena <andrescadena0607@gmail.com>
2021-09-17 16:57:46 -0500

    question entity update

--- src/entities/question.entity.ts
+++ src/entities/question.entity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Quiz } from './quiz.entity';
 
--- src/entities/quiz.entity.ts
+++ src/entities/quiz.entity.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Question } from './question.entity';
 
