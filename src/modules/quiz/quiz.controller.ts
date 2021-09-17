f9801aa90268794afa57489a8a0836a0fa3ebc92
Andres Cadena <andrescadena0607@gmail.com>
2021-09-16 20:07:26 -0500

    create controller from quiz

--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateQuizDto, UpdateQuizDto } from './dto/quiz.dto';
+import { QuizzesService } from './services/quiz.service';
 
 @Controller('quiz')
-export class QuizController {}
+export class QuizController {
+  constructor(private readonly quizzesService: QuizzesService) {}
+
+  @Post()
+  create(@Body() createQuizDto: CreateQuizDto) {
+    return this.quizzesService.create(createQuizDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.quizzesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.quizzesService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() UpdateQuizDto: UpdateQuizDto) {
+    return this.quizzesService.update(+id, UpdateQuizDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.quizzesService.delete(+id);
+  }
+}
