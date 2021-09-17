1200d081df6091042c0cec6b14b1c5464cb81bc8
Andres Cadena <andrescadena0607@gmail.com>
2021-09-17 15:19:50 -0500

    question module typeormmodule

--- src/modules/question/question.module.ts
+++ src/modules/question/question.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './services/question.service';
 import { QuestionController } from './question.controller';
+import { Question } from 'src/entities/question.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Question], 'questions')],
   providers: [QuestionsService],
   controllers: [QuestionController],
 })
