8c8a713534a3e07a474288a64a9ebc68ba814748
Andres Cadena <andrescadena0607@gmail.com>
2021-09-17 15:09:08 -0500

    question controller update

--- src/modules/question/question.controller.ts
+++ src/modules/question/question.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  CreateQuestionDto,
+  UpdateQuestionDto,
+} from '../question/dto/question.dto';
+import { QuestionsService } from '../question/services/question.service';
 
 @Controller('question')
-export class QuestionController {}
+export class QuestionController {
+  constructor(private readonly questionsService: QuestionsService) {}
+
+  @Post()
+  create(@Body() createQuizDto: CreateQuestionDto) {
+    return this.questionsService.create(createQuizDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.questionsService.findAll();
+  }
+
+  @Post(':nameQuiz')
+  createQuiz(@Param('nameQuiz') nameQuiz: string) {
+    return this.questionsService.createQuiz(nameQuiz);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.questionsService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateQuizDto: UpdateQuestionDto) {
+    return this.questionsService.update(+id, updateQuizDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.questionsService.delete(+id);
+  }
+}
